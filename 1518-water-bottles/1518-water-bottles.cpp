95485280b66e0490cad5c512035ee806601aa138
Chirag Bhundiya <107389544+ChiragGitHub231@users.noreply.github.com>
2024-07-07 14:49:03 +0530

    Time: 2 ms (53.84%), Space: 7.2 MB (30.43%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int total=0, current_full=numBottles, current_empty=0;
+        while(current_full){
+            total+=current_full;
+            current_empty+=current_full;
+            current_full=0;
+            current_full=(current_empty/numExchange);
+            current_empty%=numExchange;
+        }
+        return total;
+    }
+};
