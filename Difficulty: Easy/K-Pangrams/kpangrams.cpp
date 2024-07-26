d3a9d74907119d71c3be9b4aaa3379f742230212
Chirag Bhundiya <107389544+ChiragGitHub231@users.noreply.github.com>
2024-07-26 17:09:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/K-Pangrams/kpangrams.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+
+    bool kPangram(string str, int k) {
+        // code here
+        unordered_map<int,int>mp;
+        int length=0, unique=0;
+        
+        for(auto it:str){
+            if(it==' ') continue;
+            mp[it-'a']++;
+            if(mp[it-'a']==1) unique++;
+            length++;
+        }
+        
+        if(length<26) return false;
+        return (unique+k>=26);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        cin.ignore();
+        string str;
+        getline(cin, str);
+
+        int k;
+        cin >> k;
+
+        Solution ob;
+        bool a = ob.kPangram(str, k);
+        if (a)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
