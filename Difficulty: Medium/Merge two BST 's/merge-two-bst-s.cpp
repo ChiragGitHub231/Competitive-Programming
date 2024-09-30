36540678eaff03d4e3332597af00e7af758d543a
Chirag Bhundiya <107389544+ChiragGitHub231@users.noreply.github.com>
2024-09-30 21:43:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two BST 's/merge-two-bst-s.cpp
+++ Difficulty: Medium/Merge two BST 's/merge-two-bst-s.cpp
@@ -94,29 +94,21 @@ struct Node {
 */
 class Solution {
   public:
+    void solve(Node* root, vector<int>& ans){
+        if(!root) return;
+        solve(root->left, ans);
+        ans.push_back(root->data);
+        solve(root->right, ans);
+    }
+  
     // Function to return a list of integers denoting the node
     // values of both the BST in a sorted order.
-    void inorder(Node *root, vector<int>& ans){
-        if(root==NULL) return;
-        inorder(root->left, ans);
-        ans.push_back(root->data);
-        inorder(root->right, ans);
-        return;
-    }
-    
     vector<int> merge(Node *root1, Node *root2) {
         // Your code here
-        vector<int> arr1, arr2, ans;
-        inorder(root1, arr1);
-        inorder(root2, arr2);
-        
-        int i=0, j=0, n=arr1.size(), m=arr2.size();
-        while(i<n && j<m){
-            if(arr1[i]<=arr2[j]) ans.push_back(arr1[i++]);
-            else ans.push_back(arr2[j++]);
-        }
-        while(i<n) ans.push_back(arr1[i++]);
-        while(j<m) ans.push_back(arr2[j++]);
+        vector<int> ans;
+        solve(root1, ans);
+        solve(root2, ans);
+        sort(ans.begin(), ans.end());
         return ans;
     }
 };
