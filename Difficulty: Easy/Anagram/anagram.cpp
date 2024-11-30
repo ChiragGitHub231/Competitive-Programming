2f2d47f3a9a47499fcd9113d6818ef428d3d54fd
Chirag Bhundiya <107389544+ChiragGitHub231@users.noreply.github.com>
2024-11-30 19:59:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function is to check whether two strings are anagram of each other or not.
+    bool areAnagrams(string& s1, string& s2) {
+        // Your code here
+        vector<int> frequency(26, 0);
+        for(auto x : s1) frequency[x - 'a']++;
+        for(auto y : s2) frequency[y - 'a']--;
+        for(int x : frequency) if(x != 0) return false;
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+
+    cin >> t;
+
+    while (t--) {
+        string c, d;
+
+        cin >> c >> d;
+        Solution obj;
+        if (obj.areAnagrams(c, d))
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends
