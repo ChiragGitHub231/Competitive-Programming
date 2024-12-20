dbf338cc12a1b8a2c65f5c0d52480989000bf325
Chirag Bhundiya <107389544+ChiragGitHub231@users.noreply.github.com>
2024-12-20 21:17:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.cpp
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.cpp
@@ -6,22 +6,45 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    vector<int> spirallyTraverse(vector<vector<int> > &matrix) {
+    vector<int> spirallyTraverse(vector<vector<int> > &mat) {
         // code here
-        int n=matrix.size(), m=matrix[0].size(), s=n*m;
-        int a=0, b=0, c=n-1, d=m-1;
-        vector<int>ans;
-        while(ans.size()<s){
-            for(int i=b; i<=d && ans.size()<s; i++) ans.push_back(matrix[a][i]);
-            a++;
-            for(int i=a; i<=c && ans.size()<s; i++) ans.push_back(matrix[i][d]);
-            d--;
-            for(int i=d; i>=b && ans.size()<s; i--) ans.push_back(matrix[c][i]);
-            c--;
-            for(int i=c; i>=a && ans.size()<s; i--) ans.push_back(matrix[i][b]);
-            b++;
+        vector<int> result;
+    int n = mat.size();    // Number of rows
+    int m = mat[0].size(); // Number of columns
+    
+    int top = 0, bottom = n - 1, left = 0, right = m - 1;
+
+    while (top <= bottom && left <= right) {
+        // Traverse from left to right on the top row
+        for (int i = left; i <= right; i++) {
+            result.push_back(mat[top][i]);
         }
-        return ans;
+        top++; // Move the top boundary down
+
+        // Traverse from top to bottom on the rightmost column
+        for (int i = top; i <= bottom; i++) {
+            result.push_back(mat[i][right]);
+        }
+        right--; // Move the right boundary left
+
+        // Traverse from right to left on the bottom row (if still within bounds)
+        if (top <= bottom) {
+            for (int i = right; i >= left; i--) {
+                result.push_back(mat[bottom][i]);
+            }
+            bottom--; // Move the bottom boundary up
+        }
+
+        // Traverse from bottom to top on the leftmost column (if still within bounds)
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--) {
+                result.push_back(mat[i][left]);
+            }
+            left++; // Move the left boundary right
+        }
+    }
+
+    return result;
     }
 };
 
@@ -33,9 +56,10 @@ int main() {
     while (t--) {
         int r, c;
         cin >> r >> c;
-        vector<vector<int>> matrix(r, vector<int>(c, 0));
+        vector<vector<int>> matrix(r);
 
         for (int i = 0; i < r; i++) {
+            matrix[i].assign(c, 0);
             for (int j = 0; j < c; j++) {
                 cin >> matrix[i][j];
             }
@@ -46,6 +70,9 @@ int main() {
         for (int i = 0; i < result.size(); ++i)
             cout << result[i] << " ";
         cout << endl;
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
